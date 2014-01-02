60957ff0e702bc6d365905921e3f142057a8090a
Noa Grant <noa.grant@gmail.com>
2014-01-02 12:36:22 -0800

    production.rb changed in another attempt to fix css for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,10 +28,10 @@ Crunchm::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.compress = true
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Do not fallback to assets pipeline if a precompiled asset is missed. ########################## commented out ###########################
+  #config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
